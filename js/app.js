d78d53edbe7c5e0681772fd0bd3e01d57161226d
mohammad <taskforcemmd@gmail.com>
2025-05-08 22:40:24 +0330

    create sidebar in website

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 let loaderElem = document.querySelector('.loader')
+let menuSidebar = document.querySelector('.open')
+let sidebar = document.querySelector('.sidebar')
+
+menuSidebar.addEventListener('click',function () {
+    sidebar.classList.toggle('active')
+})
+
 
 window.addEventListener('load', function () {
     loaderElem.classList.add('hidden')
