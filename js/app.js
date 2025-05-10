ace52e678b3c40a886b37bbe03894578e06b5468
mohammad <taskforcemmd@gmail.com>
2025-05-10 12:01:18 +0330

    createa sidebar website

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 let loaderElem = document.querySelector('.loader')
 let menuSidebar = document.querySelector('.open')
+let open = document.querySelector('.open-side')
+let closeSide = document.querySelector('.close')
 let sidebar = document.querySelector('.sidebar')
 
-menuSidebar.addEventListener('click',function () {
-    sidebar.classList.toggle('active')
-})
+let sideFlag = false
 
+menuSidebar.addEventListener('click',function () {
+    if (sideFlag) {
+        sidebar.classList.remove('active')
+        closeSide.style.display = 'none'
+        open.style.display = 'flex'
+        sideFlag = false
+    } else {
+        sidebar.classList.add('active')
+        closeSide.style.display = 'flex'
+        open.style.display = 'none'
+        sideFlag = true
+    }
+
+    console.log(sideFlag)
+})
 
 window.addEventListener('load', function () {
     loaderElem.classList.add('hidden')
